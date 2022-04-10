9680deeed24979121b29b0bdb1698397b849c94b
apandey16 <apande02@calpoly.edu>
2022-04-10 00:47:07 -0700

    testing movement

--- Assets/Scenes/Area 1 Games/Platformer Assets/PlayerMovePlatformer.cs
+++ Assets/Scenes/Area 1 Games/Platformer Assets/PlayerMovePlatformer.cs
@@ -10,12 +10,18 @@ public class PlayerMovePlatformer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
-                directionMoved = Vector2.left;
-            } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
-                directionMoved = Vector2.right;
+            directionMoved = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            directionMoved = Vector2.right;
         } 
-            }
-        } 
+    }
+         
+    
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + directionMoved.x, Mathf.Round(this.transform.position.y) + directionMoved.y, 0.0f);
+    }
+}
