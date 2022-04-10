dbbd27316fa2c9bfab8108aa7a30956909aa9b98
MorrowCameron <97052268+MorrowCameron@users.noreply.github.com>
2022-04-10 02:28:21 -0700

    Text added

--- Assets/Scenes/Backgrounds.MainArea/MainCharacter.cs
+++ Assets/Scenes/Backgrounds.MainArea/MainCharacter.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class MainCharacter : MonoBehaviour
 {
+    [SerializeField]
+    public Text PongT;
+    public Text SpaceT;
+    public Text SnakeT;
+    public Text PlatT;
+
+
+    
+
     private Vector2 speed = new Vector2(0.02f,0.02f);
     // Start is called before the first frame update
     void Start()
     {
-        
+    PongT.gameObject.SetActive(false);
+    SpaceT.gameObject.SetActive(false);
+    SnakeT.gameObject.SetActive(false);
+    PlatT.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,9 +37,44 @@ public class MainCharacter : MonoBehaviour
             transform.Translate(movement);
         }
     }
-    void OnCollisionEnter2D(Collider2D other){
-        if (tag.other == "Walls"){
-            UnityEngine.Debug.Log("HIT");
+    void OnCollisionEnter2D(Collision2D other){
+        switch(other.gameObject.tag){
+            case "Pong":
+                PongT.enabled = true;
+                YesorNo("Pong");
+                PongT.enabled = false;
+                break;
+            case "Space":
+                SpaceT.enabled = true;
+                YesorNo("Space Invaders");
+                SpaceT.enabled = false;
+                break;
+            case "Snake":
+                SnakeT.enabled = true;
+                YesorNo("Snake");
+                SnakeT.enabled = false;
+                break;
+            case "Platform":
+                PlatT.enabled = true;
+                YesorNo("Platformer");
+                PlatT.enabled = false;
+                break;
+            case "OldGuy":
+                moveToEntrance();
+                break;
         }
     }
+    void moveToEntrance(){
+        transform.Translate(new Vector3(0,-3,0));
+    }
+
+    void YesorNo(string filename){
+            if(Input.GetKeyDown(KeyCode.Y)){
+                   SceneManager.LoadScene(filename);
+            }if(Input.GetKeyDown(KeyCode.N)){
+                moveToEntrance();
+            }
+    }
+    
+    
 }
