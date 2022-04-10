794ce76c0876c07e1f66a56ed8029124caaecf59
MorrowCameron <97052268+MorrowCameron@users.noreply.github.com>
2022-04-10 00:50:58 -0700

    added movement

--- Assets/Scenes/Backgrounds.MainArea/MainCharacter.cs
+++ Assets/Scenes/Backgrounds.MainArea/MainCharacter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
+    private Vector2 speed = new Vector2(0.02f,0.02f);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class MainCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement = new Vector3(speed.x*horizontal, speed.y*vertical, 0);
+
+        movement = movement - movement * Time.deltaTime;
+        if((vertical<8 && vertical>-8) && (horizontal<4 && horizontal>-4)){
+            transform.Translate(movement);
+        }
     }
 }
